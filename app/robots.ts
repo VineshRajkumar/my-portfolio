baab47b5e32075d21470d69b46c700a2f5143898
VineshRajkumar <vineshrajkumar23@gmail.com>
2025-08-01 16:38:07 +0530

    updated robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*', //<- which crawlers to target -> to target all crawlers just put * 
       allow: '/', //<- to allow all pages put only /
     },
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}sitemap.xml`, //a slash is alredy added in env so it will look correct
   }
 }
